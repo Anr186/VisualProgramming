4f7b9f1f4e15b2f6056c6b497c4f99e60a97c5cc
Anr186 <andrey.terent1ev@mail.ru>
2025-03-23 17:13:31 +0700

    search city need

--- src/App.js
+++ src/App.js
@@ -4,33 +4,74 @@ import Weather from './Weather';
 const API_KEY = '9ab4684ce216cced161fdffe709a77ff';
 
 const App = () => {
-    const [weatherData, setWeatherData] = useState([]);
+    const [weatherData, setWeatherData] = useState(null);
+    const [uvData, setUvData] = useState(null); // Состояние для UV-индекса
     const [city, setCity] = useState('Novosibirsk');
     const [location, setLocation] = useState({ lat: null, lon: null });
-    const [geoData, setGeoData] = useState(null);
+    const [currentTime, setCurrentTime] = useState(new Date());
+
     useEffect(() => {
-      const loadWeather = async () => {
-          const geoResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`);
-          const data = await geoResponse.json();
-          setGeoData(data);
+        const loadWeather = async () => {
+            const geoResp = await fetch(
+                `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`
+            );
+            const geoData = await geoResp.json();
+
+            const { lat, lon } = geoData[0];
+            setLocation({ lat, lon });
+
+            const weatherResp = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+            const weatherData = await weatherResp.json();
+            setWeatherData(weatherData);
+            
+            const uvResp = await fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+            const uvData = await uvResp.json();
+            setUvData(uvData);
         };
+
         loadWeather();
+        const weatherInterval = setInterval(loadWeather, 3 * 3600 * 1000); 
+        return () => clearInterval(weatherInterval);
     }, [city]);
 
+    useEffect(() => {
+        const timeInterval = setInterval(() => {
+            setCurrentTime(new Date()); 
+        }, 1000);
+
+        return () => clearInterval(timeInterval);
+    }, []);
+
+    const isNight = currentTime.getHours() >= 18 || currentTime.getHours() < 6;
+    const backgroundColor = isNight ? '#4e32a8' : '#1f7db8';
+
+    const formatTime = (date) => {
+        return date.toLocaleTimeString('ru-RU', {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false, 
+        });
+    };
+
     return (
-      <div>
-          <h1>Геоданные для города: {city}</h1>
-          {geoData && geoData.length > 0 ? (
-              <div>
-                  <p>Город: {geoData[0].name}</p>
-                  <p>Широта: {geoData[0].lat}</p>
-                  <p>Долгота: {geoData[0].lon}</p>
-              </div>
-          ) : (
-              <p>Загрузка геоданных...</p>
-          )}
-      </div>
-  );
-}
+        <div
+            style={{
+                textAlign: 'center',
+                padding: '20px',
+                minHeight: '100vh',
+                margin: 0,
+                backgroundColor: backgroundColor,
+            }}
+        >
+            {/* <h1>Текущее время: {formatTime(currentTime)}</h1> */}
+            {weatherData && uvData ? (
+                <Weather weatherData={weatherData} city={city} location={location} uvData={uvData} />
+            ) : (
+                <p>Загрузка данных о погоде...</p>
+            )}
+        </div>
+    );
+};
 
 export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,1 +1,117 @@
+import React from 'react';
 
+const Weather = ({ weatherData, city, location, uvData }) => {
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return date.toLocaleDateString('en-EN', { weekday: 'long', day: 'numeric' });
+    };
+
+    const currentDate = formatDate(weatherData.list[0].dt_txt);
+
+    const currentTime = new Date();
+
+    const findCurrentIndex = () => {
+        return weatherData.list.findIndex((forecast) => {
+            const forecastTime = new Date(forecast.dt_txt).getTime();
+            const timeDifference = Math.abs(forecastTime - currentTime.getTime());
+            return timeDifference <= 3600 * 1000; 
+        });
+    };
+    const currentIndex = findCurrentIndex();
+    const hourlyForecasts = weatherData.list.slice(currentIndex, currentIndex + 4);
+
+    return (
+        <div style={styles.container}>
+            <h1 style={styles.cityName}>{city}</h1>
+            <h2 style={styles.currentDate}>{currentDate}</h2>
+            <h2 style={styles.temperature}>{Math.round(hourlyForecasts[0].main.temp)}°</h2>
+            
+            <div style={styles.hourlyForecast}>
+                {hourlyForecasts.map((forecast, index) => (
+                    <div key={index} style={styles.forecastItem}>
+                        <p>{index === 0 ? 'Now' : `${new Date(forecast.dt_txt).getHours()}:00`}</p>
+                        <p>{Math.round(forecast.main.temp)}°</p>
+                    </div>
+                ))}
+            </div>
+
+            <div style={styles.weatherDetails}>
+                <div style={styles.detailItem}>
+                    <p style={styles.detailLabel}>Humidity</p>
+                    <p style={styles.detailValue}>{hourlyForecasts[0].main.humidity}%</p>
+                </div>
+                <div style={styles.detailItem}>
+                    <p style={styles.detailLabel}>Wind</p>
+                    <p style={styles.detailValue}>{Math.round(hourlyForecasts[0].wind.speed)} m/s</p>
+                </div>
+                <div style={styles.detailItem}>
+                    <p style={styles.detailLabel}>Air Pressure</p>
+                    <p style={styles.detailValue}>{hourlyForecasts[0].main.pressure} hPa</p>
+                </div>
+                <div style={styles.detailItem}>
+                    <p style={styles.detailLabel}>UV</p>
+                    <p style={styles.detailValue}>{Math.round(uvData.value)}</p>
+                </div>
+            </div>
+
+            {weatherData.list.filter((forecast, index) => (index + 1) % 8 === 0).map((forecast, index) => (
+                <div key={index} style={styles.dailyForecast}>
+                    <p>{formatDate(forecast.dt_txt)}</p>
+                    <p>{Math.round(forecast.main.temp)}°</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+const styles = {
+    container: {
+        fontFamily: 'Arial, sans-serif',
+        color: '#333',
+        padding: '20px',
+        textAlign: 'center',
+    },
+    cityName: {
+        fontSize: '2rem',
+        marginBottom: '10px',
+    },
+    currentDate: {
+        fontSize: '1.5rem',
+        margin: '10px 0',
+        color: '#666',
+    },
+    temperature: {
+        fontSize: '3rem',
+        margin: '10px 0',
+    },
+    hourlyForecast: {
+        display: 'flex',
+        justifyContent: 'space-around',
+        margin: '20px 0',
+    },
+    forecastItem: {
+        textAlign: 'center',
+    },
+    weatherDetails: {
+        display: 'flex',
+        justifyContent: 'space-around',
+        margin: '20px 0',
+    },
+    detailItem: {
+        textAlign: 'center',
+    },
+    detailLabel: {
+        fontSize: '1rem',
+        margin: '5px 0',
+        fontWeight: 'bold',
+    },
+    detailValue: {
+        fontSize: '1.2rem',
+        margin: '5px 0',
+    },
+    dailyForecast: {
+        margin: '10px 0',
+    },
+};
+
+export default Weather;
