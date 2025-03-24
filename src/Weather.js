4b54b5be48d40fad60dc43deb81dbd21cd275183
Anr186 <barashekgobo@gmail.com>
2025-03-25 00:02:56 +0700

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -13,7 +13,7 @@ const Weather = ({ weatherData, city, location, uvData, isNight, hour, isAntarct
     const hourlyForecasts = weatherData.list.slice(startIndex, startIndex + 5);
 
     const formatTime = (hour) => {
-        return `${hour % 12 === 0 ? 12 : hour % 12}:00 ${hour >= 12 ? 'PM' : 'AM'}`;
+        return ${hour}:00;
     };
 
     const formatDate = (date) => {
